88612212060dab8bfa011d1c018f1a1da25d7d4b
yujian <913401586@qq.com>
2017-10-16 19:58:19 +0800

    收文管理-接收人验证提示，人员模糊查询

--- src/components/sendDocument/indexView.js
+++ src/components/sendDocument/indexView.js
@@ -31,7 +31,6 @@ define([
             this.table.render();
             return this;
         },
-
         initSuggest: function () {
             $.each(this.$suggestWrap, function (k, el) {
                 $(el).bsSuggest('init', {
@@ -39,19 +38,45 @@ define([
                      effectiveFields: ["userName", "email"],
                      searchFields: [ "shortAccount"],
                      effectiveFieldsAlias:{userName: "姓名"},*/
-                    effectiveFieldsAlias:{userName: "姓名", userId: "ID", number: "工号"},
+                    effectiveFieldsAlias: {peopleName: "姓名", id: "ID", number: "工号"},
                     clearable: true,
                     showHeader: true,
                     showBtn: false,
-                    url: "src/components/sendDocument/data.json",
-                    idField: "userId",
-                    keyField: "userName"
+                    allowNoKeyword: false,
+                    getDataMethod: "url",
+                    delayUntilKeyup: true,
+                    // url: "src/components/sendDocument/data.json",
+                    // idField: "userId",
+                    // keyField: "userName",
+                    // url: QUERY.FUZZY_QUERY,
+                    fnAdjustAjaxParam: function (keyword, opts) {
+                        return {
+                            method: 'post',
+                            data: JSON.stringify({
+                                peopleName: keyword
+                            }),
+                            'contentType': 'application/json'
+                        };
+                    },
+                    processData: function (json) {
+                        var data = {value: []};
+                        $.each(json.data && json.data[0], function (i, r) {
+                            data.value.push({peopleName: r.peopleName, employeeNum: r.employeeNum, id: r.id})
+                        })
+                        return data;
+                    },
+                    url: QUERY.FUZZY_QUERY,
+                    idField: "id",
+                    keyField: "peopleName"
                 }).on('onDataRequestSuccess', function (e, result) {
                     console.log('onDataRequestSuccess: ', result);
                 }).on('onSetSelectValue', function (e, keyword, data) {
-                    var $operatorId = $(e.target).parents('.row').find('input[name=operatorId]');
-                    $operatorId.val(data.userId);
-                    console.log('onSetSelectValue: ', keyword, data, $operatorId.val());
+                    var $row = $(e.target).parents('.row')
+                    var $operatorId = $row.find('input[name=operatorId]');
+                    var $validInput = $row.find('.operatorId');
+                    $validInput.val(data.id);
+                    $operatorId.val(data.id);
+                    console.log('onSetSelectValue: ', keyword, data, $validInput.val(data.id), $operatorId.val());
                 }).on('onUnsetSelectValue', function () {
                     console.log('onUnsetSelectValue');
                 });
@@ -106,7 +131,7 @@ define([
 
             };
             var row = row.id ? row : initState;
-            if(row.id){
+            if (row.id) {
                 row.workFlow = JSON.parse(row.workflowData);
                 row.gmtCreate = ncjwUtil.timeTurn(row.gmtCreate);
             }
@@ -167,9 +192,27 @@ define([
                         required: true
                     },
                     content: {
-                         required: true
+                        required: true
                     },
-                    operatorId: {
+                    operator_valid1: {
+                        required: true
+                    },
+                    operator_valid2: {
+                        required: true
+                    },
+                    operator_valid3: {
+                        required: true
+                    },
+                    operator_valid4: {
+                        required: true
+                    },
+                    operator_valid5: {
+                        required: true
+                    },
+                    operator_valid6: {
+                        required: true
+                    },
+                    operator_valid7: {
                         required: true
                     },
                     comment: {
@@ -179,7 +222,13 @@ define([
                 messages: {
                     title: "请填写标题",
                     content: "请填写正文",
-                    operatorId: "请选择接收人",
+                    operator_valid1: "请选择接收人",
+                    operator_valid2: "请选择接收人",
+                    operator_valid3: "请选择接收人",
+                    operator_valid4: "请选择接收人",
+                    operator_valid5: "请选择接收人",
+                    operator_valid6: "请选择接收人",
+                    operator_valid7: "请选择接收人",
                     comment: "请填写审核意见"
                 },
                 highlight: function (element) {
@@ -214,24 +263,24 @@ define([
                 var currentOperatorId = $('#currentOperatorId').val();
                 var comment = $('#comment').val();
                 //保存
-                if(index == 0){
-                    var params = {workFlow:{nodeList: []}};
+                if (index == 0) {
+                    var params = {workFlow: {nodeList: []}};
                     $.each($inputs, function (k, el) {
                         var node = {};
                         var $el = $(el), val = $el.val(), name = $el.attr('name');
-                        if(name == "operatorId"){
+                        if (name == "operatorId") {
                             node[name] = val;
                             node.nodeName = $el.parents('.row').find('.flow-title').html();
                             params.workFlow.nodeList.push(node);
                             // }else if(name == 'status' || name == "id"){
-                        }else if(name == 'status' || name == "id"){
+                        } else if (name == 'status' || name == "id") {
 
-                        }else{
+                        } else {
                             params[name] = val;
                         }
                     })
-                }else{
-                    var params = {recordId: id, operatorId: currentOperatorId, comment: comment };
+                } else {
+                    var params = {recordId: id, operatorId: currentOperatorId, comment: comment};
                 }
 
                 ncjwUtil.postData(urlMap[index], JSON.stringify(params), function (res) {
@@ -250,11 +299,11 @@ define([
         showOrhideBtn: function (row) {
             this.$editDialog.find('.status-button').hide();
             // 创建人：新建、草稿、驳回状态显示-草稿与提交按钮
-            if(this.isCreater(row) && (row.workFlow.currentNode.nodeIndex == 0)){
+            if (this.isCreater(row) && (row.workFlow.currentNode.nodeIndex == 0)) {
                 this.$editDialog.find("#btn-draft,#btn-submit").show();
             }
             // 处理人：已提交状态显示-通过与驳回按钮
-            if(this.isOpertor(row) && (row.workFlow.currentNode.nodeIndex != 0)){
+            if (this.isOpertor(row) && (row.workFlow.currentNode.nodeIndex != 0)) {
                 this.$editDialog.find("#btn-ok,#btn-no").show();
             }
             // this.$editDialog.find('.status-button').hide();
